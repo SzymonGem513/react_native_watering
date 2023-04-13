5ca7b85988d078647ef348e54da01c53c7473bbf
ka1x <kath021@interia.pl>
2023-04-13 13:43:22 +0200

    working time picker wip

--- app/components/PopupScreen.js
+++ app/components/PopupScreen.js
@@ -1,17 +1,83 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
 	SafeAreaView,
 	Text,
 	TouchableOpacity,
 	View,
 	Modal,
- } from "react-native";
+} from "react-native";
 
-const PopupScreen = ({isModalVisible, toggleModal}) => {
-  return (
-<>
+import {styles} from '../styles/PopupStyles'
 
-<Modal visible={isModalVisible} animationType="slide">
+const PopupScreen = ( {isModalVisible, toggleModal} ) => {
+
+	const [hour, setHour] = useState( 12 );
+	const [minute, setMinute] = useState( 0 );
+	const [isAM, setIsAM] = useState( true );
+
+	const toggleAMPM = () => {
+		setIsAM( !isAM );
+	};
+
+	const formatNumber = ( number ) => {
+		return number < 10 ? '0' + number : number;
+	};
+
+	const handleHourIncrement = () => {
+		setHour( ( hour + 1 ) % 12 || 12 );
+	};
+
+	const handleHourDecrement = () => {
+		setHour( ( hour + 11 ) % 12 || 12 );
+	};
+
+	const handleMinuteIncrement = () => {
+		setMinute( ( minute + 1 ) % 60 );
+	};
+
+	const handleMinuteDecrement = () => {
+		setMinute( ( minute + 59 ) % 60 );
+	};
+
+
+	return (
+		<>
+			<Modal visible={isModalVisible} animationType="slide">
+
+					<View style={styles.timeContainer}>
+						<TouchableOpacity onPress={handleHourIncrement}>
+							<Text style={styles.timeText}>{formatNumber( hour )}</Text>
+						</TouchableOpacity>
+						<Text style={styles.timeText}>:</Text>
+						<TouchableOpacity onPress={handleMinuteIncrement}>
+							<Text style={styles.timeText}>{formatNumber( minute )}</Text>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={toggleAMPM}>
+							<Text style={styles.timeText}>{isAM ? 'AM' : 'PM'}</Text>
+						</TouchableOpacity>
+					</View>
+					<View style={styles.buttonContainer}>
+						<TouchableOpacity onPress={handleHourIncrement}>
+							<Text style={styles.buttonText}>+</Text>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={handleHourDecrement}>
+							<Text style={styles.buttonText}>-</Text>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={handleMinuteIncrement}>
+							<Text style={styles.buttonText}>+</Text>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={handleMinuteDecrement}>
+							<Text style={styles.buttonText}>-</Text>
+						</TouchableOpacity>
+					</View>
+
+
+				<TouchableOpacity onPress={toggleModal}>
+					<Text>Close</Text>
+				</TouchableOpacity>
+
+			</Modal>
+			{/* <Modal visible={isModalVisible} animationType="slide">
           <View
             style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
           >
@@ -20,9 +86,9 @@ const PopupScreen = ({isModalVisible, toggleModal}) => {
               <Text>Close</Text>
             </TouchableOpacity>
           </View>
-        </Modal>
+        </Modal> */}
 
-</>
+		</>
 	)
 }
 
--- app/styles/PopupStyles.js
+++ app/styles/PopupStyles.js
@@ -1,5 +1,24 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-
+	container: {
+		alignItems: 'center',
+	 },
+	 timeContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	 },
+	 timeText: {
+		fontSize: 32,
+		marginHorizontal: 10,
+	 },
+	 buttonContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginTop: 10,
+	 },
+	 buttonText: {
+		fontSize: 24,
+		paddingHorizontal: 10,
+	 },
 });
